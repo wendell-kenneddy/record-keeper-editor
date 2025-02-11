b54ea8e08e83343e83a729946c64da35d931b0b5
Wendell Kenneddy <wkenneddy505@gmail.com>
2025-02-11 19:34:31 -0300

    feat(layout): add icon related meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,10 +7,21 @@ import {
 } from "@mantine/core";
 import { DatesProvider } from "@mantine/dates";
 import { Header } from "@/components/header";
+import { Metadata } from "next";
 
 import "@mantine/core/styles.css";
 import "@mantine/tiptap/styles.css";
 import "@mantine/dates/styles.css";
+
+export const metadata: Metadata = {
+  creator: "Wendell Kenneddy",
+  icons: [
+    { rel: "icon", url: "/favicon.ico", sizes: "any" },
+    { rel: "icon", url: "/icon64.svg", type: "image/svg+xml" },
+    { rel: "apple-touch-icon", url: "/apple-touch-icon.png", type: "image/png" },
+  ],
+  other: { "theme-color": "#1f1f1f" },
+};
 
 export default function RootLayout({
   children,
