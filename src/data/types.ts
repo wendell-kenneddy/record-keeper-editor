b8a9ada48e01b3d5e45f225605beb69e52f14f3a
Wendell Kenneddy <wkenneddy505@gmail.com>
2025-02-11 18:17:57 -0300

    feat(types): add more types

--- src/data/types.ts
+++ src/data/types.ts
@@ -7,3 +7,31 @@ export const LOCALIZED_RELEASE_TYPE_NAMES = {
 export type ReleaseType = "single" | "album" | "ep";
 
 export type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
+
+export interface Artist {
+  id: number;
+  name: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface Release {
+  id: number;
+  title: string;
+  artist: string;
+  artistId: number;
+  length: number;
+  releaseType: ReleaseType;
+  releasedAt: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface Song {
+  id: number;
+  title: string;
+  artist: string;
+  release: string;
+  createdAt: string;
+  updatedAt: string;
+}
