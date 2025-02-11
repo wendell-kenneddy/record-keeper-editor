4f61b6b113b35bebda6cb193254ba37b30d73685
Wendell Kenneddy <wkenneddy505@gmail.com>
2025-02-11 18:27:54 -0300

    feat(components/header): update links

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,15 +1,29 @@
-import { Center, Flex } from "@mantine/core";
+import { Center, Group, Text } from "@mantine/core";
 import Image from "next/image";
 import Link from "next/link";
 
 export function Header() {
   return (
-    <Center w="full" h={64} bg="dark">
-      <Flex w="100%" maw={1024} align="center" justify="space-between">
+    <Center py="lg" bg="dark">
+      <Group w="90%" maw={1024} align="center" justify="space-between">
         <Link href="/editor/songs" style={{ display: "flex", alignItems: "center" }}>
           <Image src="/logo.svg" alt="Logo" width={106} height={19} priority />
         </Link>
-      </Flex>
+
+        <Group align="center">
+          <Text size="sm">
+            <Link href="/editor/releases" style={{ color: "inherit", textDecoration: "none" }}>
+              Lançamentos
+            </Link>
+          </Text>
+
+          <Text size="sm">
+            <Link href="/editor/artists" style={{ color: "inherit", textDecoration: "none" }}>
+              Artistas
+            </Link>
+          </Text>
+        </Group>
+      </Group>
     </Center>
   );
 }
