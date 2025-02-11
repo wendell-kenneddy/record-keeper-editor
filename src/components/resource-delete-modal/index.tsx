4be9980e555a658297e4689b9115f3c8bc1a74a6
Wendell Kenneddy <wkenneddy505@gmail.com>
2025-02-11 18:19:11 -0300

    feat(components): add resource-delete-modal component

---
+++ src/components/resource-delete-modal/index.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { Button, Group, Modal, Stack, Text } from "@mantine/core";
+
+interface ResourceDeleteModalProps {
+  opened: boolean;
+  onClose: () => void;
+  onConfirm: () => void;
+  title: string;
+  description: string;
+}
+
+export function ResourceDeleteModal({
+  opened,
+  onConfirm,
+  onClose,
+  title,
+  description,
+}: ResourceDeleteModalProps) {
+  return (
+    <Modal centered opened={opened} onClose={close} title={title}>
+      <Stack align="flex-start">
+        <Text size="sm">{description}</Text>
+
+        <Group w="100%" align="center" justify="space-between">
+          <Button w="48%" variant="filled" color="teal" onClick={onClose}>
+            Cancelar
+          </Button>
+
+          <Button w="48%" variant="outline" color="red" onClick={onConfirm}>
+            Confirmar
+          </Button>
+        </Group>
+      </Stack>
+    </Modal>
+  );
+}
